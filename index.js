d8bd798c28fcf16e991000717a7996a109a56b54
sadikaRimu <sadika3046@gmail.com>
2022-11-08 16:24:43 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+//middle wares
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('food server is running');
+});
+app.listen(port, () => {
+    console.log(`food server is running on the port${port}`);
+})
